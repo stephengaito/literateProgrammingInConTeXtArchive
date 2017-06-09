5d8166d5e58be8a7393531504ef48648932a7a68
Stephen Gaito <stephen@perceptisys.co.uk>
2017-06-09 15:49:31 +0100

    reverted mkiv file creation template while re-implementing rendering

--- t-literate-progs/tex/context/third/literateProgs/t-literate-progs-templates.lua
+++ t-literate-progs/tex/context/third/literateProgs/t-literate-progs-templates.lua
@@ -24,15 +24,8 @@ templates.litProgsTable = {}
 local table_insert = table.insert
 local table_concat = table.concat
 
-function templates.concat(aTable)
-  return table_concat(aTable, '\n\n')
-end
-
---addTemplate {
---  'mkivFile',
---  { }}
 templates.mkiv.file = [=[
-{{! concat(thirddata.literateProgs.code.mkiv, "\n\n") }}
+{{return table.concat(thirddata.literateProgs.code.mkiv, "\n\n") }}
 ]=]
 
 templates.lua.file = [=[
